36dd5b3a52aaf2b55c163a295975f0d2547c28b6
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-08 19:44:09 +0300

    added tailwind and daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import Login from './components/Login/Login';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,68 @@
+import { Field, Form, Formik } from 'formik';
+
+const Login = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  const handleSubmit = values => {
+    console.log(values);
+  };
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur,
+            vitae eveniet voluptas, sapiente debitis sequi enim consequuntur
+            accusantium distinctio accusamus architecto blanditiis officia cum
+            delectus fuga hic veniam, beatae deserunt.
+          </p>
+        </div>
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <Formik>
+            <Form
+              initialValues={initialValues}
+              onSubmit={handleSubmit}
+              className="card-body"
+            >
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <Field
+                  name="email"
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
+              </div>
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
